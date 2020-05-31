43a9c147308aa71b1f23ad206fec94bed4c7e9e3
huajianlei0306 <44830612+huajianlei0306@users.noreply.github.com>
2020-05-31 14:12:07 +0800

    我的测试上传

---
+++ CitysSpeedService.java
@@ -0,0 +1,17 @@
+package com.fjut.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 地区平均速度排行
+ */
+@Component
+public interface CitysSpeedService {
+
+    /**
+     * 地区平均速度排行
+     * rank_citys_speed
+     */
+    public void paoyouCitysSpeedServiceRanking();
+
+}
